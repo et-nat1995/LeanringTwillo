da69326604193dab9ec5fe42cf8241abcdd4ffff
Tamir <et.natanov@gmail.com>
2019-01-21 12:54:55 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const client = require("twilio")(sid, auth);
+
+app.post("/api/send", function (req, res) {
+    console.log(req.body);
+    client.messages.create({
+        body: 'Hello',
+        from: number,
+        to: "+14804402748"
+    }).then(message => {
+        res.send(message);
+    })
+})
+
+app.post("/api/getMessage", function (req, res) {
+    const twiml = new MessagingResponse();
+
+    // if (req.body.Body === "Hello there") {
+    //     twiml.message("Hi!!");
+    // }
+    // else {
+    //     twiml.message("I got nothing to say to that for now.");
+    // }
+
+    // res.writeHead(200, { 'Content-Type': 'text/xml' });
+    // res.end(twiml.toString());
+
+    client.messages.create({
+        body: "Someone sent you a message: " + req.body.From + "\nThere message was: " + req.body.Body,
+        from: number,
+        to: "+14804402748"
+    });
+})
+
+app.listen(PORT, function () {
+    console.log(`App listening on PORT ${PORT}`);
+});
